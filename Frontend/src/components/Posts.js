065156e37a7418fe433faa9f65e9622ce53a0293
Guru <shubhamka13@gmail.com>
2023-10-23 15:00:10 +0530

    pagination design

--- Frontend/src/components/Posts.js
+++ Frontend/src/components/Posts.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 const Posts = ({ posts, loading }) => {
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
+    if (loading) {
+        return <h2>Loading...</h2>;
+    }
 
-  return (
-    <ul className='list-group mb-4'>
-      {posts.map(post => (
-        <li key={post.id} className='list-group-item'>
-          {post.title}
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+
+        <>
+
+            <table className='recipeTable'>
+                <thead>
+                    <tr>
+                        <th>Recipes Name</th>
+                        <th>Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {posts.map(post => (
+                        <tr key={post.id}>
+                            <td>{post.title}</td>
+                            <td>{post.id}</td>
+                            <td>Action</td>
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
+
+
+        </>
+
+    );
 };
 
 export default Posts;
